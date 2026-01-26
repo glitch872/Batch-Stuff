10971fa390c14657df64c87b671601e4528e7e91
glitch872 <86266768+glitch872@users.noreply.github.com>
2026-01-25 21:24:30 -0800

    Refactor setup.bat and update download links
    Refactor setup.bat to improve readability and update URLs.

--- cmd-custom-terminal/setup.bat
+++ cmd-custom-terminal/setup.bat
@@ -1,20 +1,7 @@
-REM curl -o "setup.bat" "https://www.glitch-bypass.com/terminal/setup.bat" & setup.bat @ exit
-
 @echo off
+:: Sorry about readability, I am not good at making code legible
 
-REM cd %userprofile% & cd .. & cd ..
-REM IF EXIST "frost" (
-REM   rmdir frost /S /Q
-REM   cd %userprofile% & cd Desktop & cd OneDrive & cd Desktop
-REM   del Launcher.exe
-REM )
-
-
-
-rem Old:
-rem curl -o "setup.bat" "https://glitch872.github.io/setup.txt" & setup.bat & exit
-
-
+:: setup
 
 color 0A
 title update/install
@@ -29,21 +16,42 @@ md .bat
 cd .bat
 cls
 
+:: CMD.bat
+
 echo @echo off>CMD.bat
+echo. >>CMD.bat
+echo :: Check if first run>>CMD.bat
+echo. >>CMD.bat
+echo cd %%userprofile%%>>CMD.bat
+echo IF EXIST first-run-bat (>>CMD.bat
+echo 	goto installed>>CMD.bat
+echo )>>CMD.bat
+echo.>>CMD.bat
+echo :: Ask whether to auto-update>>CMD.bat
+echo. >>CMD.bat
+echo echo Auto-Update? (Default : Y)>>CMD.bat
+echo set /p c=Y/n: >>CMD.bat
+echo if /i "%%c%%"=="n" goto no-update >>CMD.bat
+echo goto update>>CMD.bat
+echo :no-update >>CMD.bat
+echo echo .^>no-update>>CMD.bat
+echo :update>>CMD.bat
+echo. >>CMD.bat
+echo echo .^>first-run-bat>>CMD.bat
+echo cd Desktop>>CMD.bat
+echo cd "Art Project">>CMD.bat
+echo cd .bat>>CMD.bat
+echo cls>>CMD.bat
+echo. >>CMD.bat
+echo :installed >>CMD.bat
 echo cd %%userprofile%%>>CMD.bat
 echo color 0A>>CMD.bat
-
-rem echo doskey lc=git clone --recursive --depth 50 https://github.com/winstxnhdw/lc-hax.git$tcd lc-hax$tstart requirements.bat$texplorer %%cd%%$tcls>>CMD.bat
-
 echo doskey auto-update=goto auto-update>>CMD.bat
-
-echo doskey repair=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -s -o "a.bat" "https://www.glitch-bypass.com/terminal/install.bat"$tstart a.bat$tcls$tcurl -s -o "b.bat" "https://www.glitch-bypass.com/terminal/setup.bat"$tb.bat$texit>>CMD.bat
-
-echo doskey update-all=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -s -o "a.bat" "https://www.glitch-bypass.com/terminal/update.bat"$tstart a.bat$tcls>>CMD.bat
-echo doskey update=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -s -o "a.bat" "https://www.glitch-bypass.com/terminal/setup.bat"$ta.bat$texit>>CMD.bat
+echo doskey repair=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/install.bat"$tstart a.bat$tcls$tcurl -L -s -o "b.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/setup.bat"$tb.bat$texit>>CMD.bat
+echo doskey update-all=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/update.bat"$tstart a.bat$tcls>>CMD.bat
+echo doskey update=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/setup.bat"$ta.bat$texit>>CMD.bat
 echo doskey trollmode=goto troll>>CMD.bat
-echo doskey old=curl -s -o "setup.bat" "https://glitch872.github.io/setup.txt"$tsetup.bat$texit>>CMD.bat
-echo doskey min=curl -s -o "setup.bat" "https://www.glitch-bypass.com/terminal/min.bat"$tsetup.bat$texit>>CMD.bat
+echo doskey min=curl -L -s -o "setup.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/min.bat"$tsetup.bat$texit>>CMD.bat
 echo doskey ?=goto 3 >>CMD.bat
 echo doskey ~=cd %%userprofile%%$tcls>>CMD.bat
 echo doskey ~1=cd %%appdata%%$tcls>>CMD.bat
@@ -57,17 +65,11 @@ echo doskey ~2=cd %%System32%%$tcls>>CMD.bat
 echo doskey ~~=goto 2 >>CMD.bat
 echo doskey Boot=goto Boot >>CMD.bat
 echo doskey uninstall=cd %%userprofile%%$tdel first-run-bat$tdel no-update$tcd Desktop$trd /s /q "Art Project"$tcd OneDrive$tcd Desktop$tdel *SCMD.exe$texit>>CMD.bat
-echo doskey no-admin=goto no-admin >>CMD.bat
+echo doskey user-level=goto no-admin >>CMD.bat
 echo doskey find=goto find>>CMD.bat
 echo doskey ls=dir /w>>CMD.bat
-echo doskey spam=goto spam>>CMD.bat
 echo doskey clear=cls>>CMD.bat
-echo doskey Discord=goto Discord>>CMD.bat
-echo doskey hide=goto hide >>CMD.bat
-echo doskey exe=curl -s -o "bat2exe.bat" "https://www.glitch-bypass.com/terminal/yep.bat"$tgoto 2 >>CMD.bat
-echo doskey trash=cd %%userprofile%%$tcurl -s -o "setup.bat" "https://www.glitch-bypass.com/terminal/blood.bat"$tsetup.bat$texit>>CMD.bat
 echo cd %%userprofile%%>>CMD.bat
-
 echo IF EXIST no-update (>>CMD.bat
 echo     goto skip>>CMD.bat
 echo )>>CMD.bat
@@ -75,11 +77,10 @@ echo IF EXIST update-bat (>>CMD.bat
 echo     goto skip>>CMD.bat
 echo )>>CMD.bat
 echo echo .^>update-bat>>CMD.bat
-echo curl -s -o "setup.bat" "https://www.glitch-bypass.com/terminal/setup.bat">>CMD.bat
+echo curl -L -s -o "setup.bat" "https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/setup.bat">>CMD.bat
 echo setup.bat>>CMD.bat
 echo :skip>>CMD.bat
 echo del update-bat>>CMD.bat
-
 echo cls>>CMD.bat
 echo ver^>ver.txt>>CMD.bat
 echo for /f "skip=1 delims=" %%%%v in (ver.txt) do (>>CMD.bat
@@ -92,14 +93,14 @@ echo echo.>>CMD.bat
 echo echo {GlitchPrompt by Glitch872}>>CMD.bat
 echo echo.>>CMD.bat
 echo title Glitch Prompt>>CMD.bat
-
 echo :1>>CMD.bat
 echo color 0a>>CMD.bat
 echo set a=>>CMD.bat
 echo set /p a= %%cd%%^^^>>>CMD.bat
+echo title Glitch Prompt - %%a%%>>CMD.bat
 echo %%a%%>>CMD.bat
+echo title Glitch Prompt>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :troll>>CMD.bat
 echo doskey cls=echo Nah, use clearterminal>>CMD.bat
 echo doskey clearterminal=cls>>CMD.bat
@@ -111,12 +112,10 @@ echo doskey ipconfig=echo Nah, use internet-protocol-configuration>>CMD.bat
 echo doskey internet-protocol-configuration=ipconfig>>CMD.bat
 echo cls>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :2>>CMD.bat
 echo explorer %%cd%%>>CMD.bat
 echo cls>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :3 >>CMD.bat
 echo cls>>CMD.bat
 echo help>>CMD.bat
@@ -125,13 +124,12 @@ echo echo ~              Jump to the userprofile directory>>CMD.bat
 echo echo ~1             Jump to the appdata directory>>CMD.bat
 echo echo ~2             Jump to the system32 directory>>CMD.bat
 echo echo ~~             Opens File Explorer to the directory you are in>>CMD.bat
-echo echo no-admin       Skip admin required (sometimes)>>CMD.bat
+echo echo user-level     Skip admin required (sometimes)>>CMD.bat
 echo echo update         Update the file to the latest version>>CMD.bat
 echo echo uninstall      Uninstall the terminal and all related files>>CMD.bat
 echo echo auto-update    Toggle auto-update>>CMD.bat
 echo echo ?              Custom commands list>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :auto-update >>CMD.bat
 echo IF EXIST no-update (>>CMD.bat
 echo     del no-update>>CMD.bat
@@ -143,14 +141,12 @@ echo echo auto-update disabled>>CMD.bat
 echo :update-toggle>>CMD.bat
 rem echo pause^>^>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :Boot>>CMD.bat
 echo cd %%userprofile%%>>CMD.bat
 echo cd Desktop>>CMD.bat
 echo cd Art Project\.bat>>CMD.bat
 echo call CMD.bat>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :no-admin >>CMD.bat
 echo cls>>CMD.bat
 echo dir /W>>CMD.bat
@@ -160,88 +156,59 @@ echo cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %%file-dir%%">>CMD
 echo pause>>CMD.bat
 echo cls>>CMD.bat
 echo goto 1 >>CMD.bat
-
 echo :find>>CMD.bat
 echo set /p a= File-Name: >>CMD.bat
 echo dir /s "C:\%%a%%">>CMD.bat
 echo pause>>CMD.bat
 echo goto 1 >>CMD.bat
 
-echo :spam>>CMD.bat
-echo set /p a=IP/Web Address: >>CMD.bat
-
-echo :spam-2>>CMD.bat
-echo ping %%a%% -n 1 >>CMD.bat
-echo goto spam-2>>CMD.bat
-
-echo :Discord>>CMD.bat
-echo set save=%%cd%%>>CMD.bat
-echo cd %%userprofile%%>>CMD.bat
-echo cd ..>>CMD.bat
-echo cd ..>>CMD.bat
-echo FOR /F "delims=" %%%%F IN ('dir /S /b discord.exe') DO SET ExePath=%%%%F>>CMD.bat
-echo cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%%ExePath%%"">>CMD.bat
-echo cd %%save%%>>CMD.bat
-echo goto 1 >>CMD.bat
-
-echo :hide>>CMD.bat
-echo cls>>CMD.bat
-echo pause^>nul>>CMD.bat
-echo goto 1 >>CMD.bat
-
-
+:: log.bat
 
 echo @echo off>log.bat
 echo title File compiler list>>log.bat
-echo echo Compiling>>log.bat
 echo cd %%userprofile%%>>log.bat
+echo mkdir dir>>log.bat
 echo cd ..>>log.bat
 echo cd ..>>log.bat
-echo dir /s %%cd%%*^>info.txt>>log.bat
+echo cls>>log.bat
+echo echo Compiling>>log.bat
+echo dir /s %%cd%%*^>%%userprofile\dir\info.txt>>log.bat
+echo explorer %%userprofile%%\dir\>>log.bat
 echo cls>>log.bat
 echo echo done>>log.bat
 echo pause>>log.bat
 
-
-
-echo @echo off>Help.bat
-echo cls>>Help.bat
-echo echo In CMD.bat, there are a few custom commands>>Help.bat
-echo echo the command ~ Brings you to the userprofile directory>>Help.bat
-echo echo the command ~1 Brings you to the appdata directory>>Help.bat
-echo echo the command ~2 Brings you to the system32 directory>>Help.bat
-echo echo the command ~~ Opens File Explorer to the directory you are in>>Help.bat
-echo echo I made this so that there is ease of access to these main file directories>>Help.bat
-echo echo using "no-admin" you can run a file without needing admin (will only work with some files)>>Help.bat
-echo echo use "update" to update the file to the latest version>>Help.bat
-echo echo use "uninstall" to uninstall all related files>>Help.bat
-echo echo use "auto-update" to toggle auto-update>>Help.bat
-echo echo (upon starting CMD.bat, it will auto-update)>>Help.bat
-echo echo You can re-open this again by typing ? into CMD.bat>>Help.bat
-echo pause>>Help.bat
+:: Check if first run
 
 cd %userprofile%
 IF EXIST first-run-bat (
-	goto skip
+    goto skip
 )
 
-echo Auto-Update? (Default : Y)
-set /p c=y/n: 
-if /i "%c%"=="n" goto no-update
-goto update
-:no-update
-echo .>no-update
-:update
-
-echo .>first-run-bat
-cd Desktop
-cd "Art Project"
-cd .bat
-cls
+:: Finalize
 
 echo Done with setup
 pause
-call Help
+
+:: Help page on first run
+
+cls
+echo In CMD.bat, there are a few custom commands
+echo the command ~ Brings you to the userprofile directory
+echo the command ~1 Brings you to the appdata directory
+echo the command ~2 Brings you to the system32 directory
+echo the command ~~ Opens File Explorer to the directory you are in
+echo I made this so that there is ease of access to these main file directories
+echo using "user-level" you can run a file without needing admin (will only work with some files)
+echo use "update" to update the file to the latest version
+echo use "uninstall" to uninstall all related files
+echo use "auto-update" to toggle auto-update
+echo (upon starting CMD.bat, it will auto-update)
+echo You can re-open this again by typing ? into CMD.bat
+pause
+
+:: Run CMD.bat
+
 :skip
 cd Desktop
 cd "Art Project"
@@ -249,4 +216,7 @@ cd .bat
 rem explorer %cd%
 start CMD
 del Help.bat
+
+:: Delete self when done
+
 (goto) 2>nul & del "%~f0" & exit
