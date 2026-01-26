13f5da364ce25d1902ab484486988b0dbabecd05
glitch872 <86266768+glitch872@users.noreply.github.com>
2026-01-25 21:28:11 -0800

    Fix URLs and clean up update.bat script
    Updated URLs to point to the new GitHub repository for setup and yep scripts. Removed commented-out lines for clarity.

--- cmd-custom-terminal/update.bat
+++ cmd-custom-terminal/update.bat
@@ -14,10 +14,6 @@ echo :: Hello, why you looking here? >>SCMD.bat
 rem New Code
 echo cd Desktop>>SCMD.bat
 echo cls>>SCMD.bat
-:: echo cd OneDrive>>SCMD.bat
-echo cls>>SCMD.bat
-:: echo cd Desktop>>SCMD.bat
-echo cls>>SCMD.bat
 echo cd "Art Project">>SCMD.bat
 echo cls>>SCMD.bat
 echo cd .bat>>SCMD.bat
@@ -29,7 +25,7 @@ echo IF EXIST CMD.bat (>>SCMD.bat
 echo   cd %%userprofile%%^>nul>>SCMD.bat
 echo   goto skip>>SCMD.bat
 echo )>>SCMD.bat
-echo curl -s -o a.bat https://www.glitch-bypass.com/terminal/setup.bat^>nul>>SCMD.bat
+echo curl -s -o a.bat https://https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/setup.bat^>nul>>SCMD.bat
 echo a.bat>>SCMD.bat
 echo exit>>SCMD.bat
 echo :skip>>SCMD.bat
@@ -48,21 +44,12 @@ echo echo Checking for updates>>SCMD.bat
 echo FOR /F "delims=" %%%%F IN ('dir /S /b CMD.bat') DO SET ExePath=%%%%F>>SCMD.bat
 echo cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%%ExePath%%"">>SCMD.bat
 
-rem curl -s -o "setup.bat" "https://www.glitch-bypass.com/terminal/setup.bat"
+rem curl -s -o "setup.bat" "https://https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/setup.bat"
 rem start setup.bat
 
-curl -s -o exe.bat https://www.glitch-bypass.com/terminal/yep.bat
+curl -s -o exe.bat https://https://raw.githubusercontent.com/glitch872/Batch-Stuff/refs/heads/main/cmd-custom-terminal/yep.bat
 call exe.bat SCMD.bat
 del exe.bat
 del SCMD.bat
-
-rem curl -o ico.bat https://www.glitch-bypass.com/terminal/applyico.bat
-rem curl -o img.ico https://www.glitch-bypass.com/terminal/glitch-terminal.ico
-rem start ico.bat "SCMD.exe" "img.ico"
-rem del ico.bat
-
-goto o
-echo taskkill /FI "WINDOWTITLE eq exe-maker" /F>a.bat & echo (goto) 2^^^>nul ^^^& del "%%%%~f0"^>a.bat ^& a.bat>>a.bat & echo echo This is designed to not work for a reason, just run SCMD.bat to finish the install>>a.bat & echo pause^>nul>>a.bat & a.bat
-:o
 
 (goto) 2>nul & del "%~f0"
